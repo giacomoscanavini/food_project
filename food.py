283f27565905f6221b6969f2860a585fc7536f6e
giacomoscanavini <giacomo.scanavini@yale.edu>
2024-05-07 18:22:03 -0400

    fixed some bug in food_database

--- food.py
+++ food.py
@@ -4,10 +4,23 @@ import re
 import json
 import datetime
 import os
+import matplotlib.pyplot as plt
 
 
 database_path = "/mnt/c/Users/scana/Dropbox/FoodProject/food_database.csv"
 diary_path = "/mnt/c/Users/scana/Dropbox/FoodProject/diary_food/"
+
+
+def load_food_database(file_path=database_path):
+    df_food = pd.read_csv(file_path)
+    # Convert numpy.float64('nan') to np.nan explicitly (redundant but safe for demonstration)
+    for col in df_food.columns:
+        df_food[col] = df_food[col].apply(lambda x: np.nan if pd.isna(x) else x)
+    return df_food
+
+
+def save_df(df, file_path=database_path):
+    df.to_csv(file_path, index=False)
 
 
 def generate_new_day(
@@ -49,30 +62,28 @@ def generate_new_day(
         print(f"File {filename} already exists.")
 
 
-# function that search for entries with a specific word, prints nutrients
-def find_food(df=None, food_name=None):
+def find_food(df_food=None, food_name=None, exact=False):
     """
-    This function searches the food database to see if there is a match, if so it prints the database entry
+    This function searches the food database to see if there is a match, if so it prints the database entry. This function doesn't reset the index of the filtered pandas.DataFrame
 
     Parameters:
-        df (pd.DataFrame): Food database
+        df_food (pd.DataFrame): Food database
         food_name (str): String to search in pd.DataFrame
+        exact (bool): Determine how strict the search is, whether contained in or exact match
 
     Returns:
         pd.DataFrame: Filtered pd.DataFrame with string contained in the food name
 
     """
-
-    # for i in range(df_food.Name.shape[0]):
-    # if 'cheese' in df_food.Name.loc[i]:
-    #    df_food.loc[i]
-
-    mask = df["Name"].isin([food_name])
-    return df[mask]
+    if exact:
+        mask = df_food["Name"] == food_name
+    else:
+        mask = df_food["Name"].str.contains(food_name, case=False, na=False)
+    return df_food[mask]
 
 
 # function that imports a diary entry, cretes a dataframe and ?
-def parse_diary_entry(file_name):
+def diary_entry_to_df(file_name):
     """
     Parses food entries from a text file into a pandas DataFrame.
 
@@ -119,10 +130,20 @@ def parse_diary_entry(file_name):
     return df_diary
 
 
-# function that sums up a diary day
-def convert_diary_entry_to_nutrients(df_diary, df_database):
-    """ """
+def nutrients_to_df(df_diary, df_food):
+    """
+    This function generates a pandas.DataFrame with all nutrients eaten given a starting pandas.DataFrame of food and quantity eaten
 
+    Parameters:
+        df_diary (pd.DataFrame): DataFrame with food eaten and quantities
+        df_food (pd.DataFrame): Food database
+        file_name (str): The name of the text file to be parsed.
+
+    Returns:
+        pd.DataFrame: DataFrame with all food eaten converted to nutrients and scaled according to eaten quantities
+    """
+
+    # Initialize output DataFrame
     cols = [
         "Name",
         "Calories",
@@ -134,10 +155,13 @@ def convert_diary_entry_to_nutrients(df_diary, df_database):
         "Dietary_fibers",
         "Sugars",
         "Proteins",
+        "Meal",
+        "Eaten",
+        "Scaling",
     ]
-
     df_nutrients = pd.DataFrame(columns=cols)
 
+    # Initialize output DataFrame entry
     dict_nutrients = {
         "Name": None,
         "Calories": None,
@@ -149,83 +173,162 @@ def convert_diary_entry_to_nutrients(df_diary, df_database):
         "Dietary_fibers": None,
         "Sugars": None,
         "Proteins": None,
+        "Meal": None,
+        "Eaten": None,
+        "Scaling": None,
     }
+
+    # Loop over eaten food as reported into the diary
     for i in range(df_diary.shape[0]):
-        eaten_size = df.loc[i]["Size"]
-        eaten_unit = df.loc[i]["Unit"]
-        eaten_name = df.loc[i]["Name"]
-        eaten_meal = df.loc[i]["Meal"]
+        # Store eaten food info
+        eaten_size = float(df_diary.loc[i]["Size"])
+        eaten_unit = str(df_diary.loc[i]["Unit"])
+        eaten_name = str(df_diary.loc[i]["Name"])
+        eaten_meal = str(df_diary.loc[i]["Meal"])
 
-        mask = df_database["Name"].isin([eaten_name])
-        df_dummy = df_database[mask]
-
-        Name = df_dummy["Name"]
-        Size = df_dummy["Size"]
-        Size_unit = df_dummy["Size_unit"]
-        Size_g = df_dummy["Size_g"]
-        Calories = df_dummy["Calories"]
-        Total_fats = df_dummy["Total_fats"]
-        Saturated_fats = df_dummy["Saturated_fats"]
-        Cholesterol = df_dummy["Cholesterol"]
-        Sodium = df_dummy["Sodium"]
-        Total_carbs = df_dummy["Total_carbs"]
-        Dietary_fibers = df_dummy["Dietary_fibers"]
-        Sugars = df_dummy["Sugars"]
-        Proteins = df_dummy["Proteins"]
-
-        # Unit in eaten list is not "g"
-        if eaten_unit != "g":
-            # Units is the one used in the food database --> Use Size
-            if eaten_unit == Size_unit:
-                scale = eaten_size / Size
-            # No match --> Error
-            else:
-                print(f"WARNING UNIT: eaten = {eaten_unit} vs database = {Size_unit}")
-                continue
-        # Unit in eaten list is "g"
+        # print(eaten_size, eaten_unit, eaten_name, eaten_meal)
+        # Find entry in food database
+        df_dummy = find_food(df_food, eaten_name, exact=True).reset_index(drop=True)
+        # Check on the eaten unit to understand if "g" or another unit is reported
+        # If eaten : g, use food database Size_g for scaling factor
+        if eaten_unit == "g":
+            print(eaten_name)
+            scale = eaten_size / float(df_dummy.loc[0]["Size_g"])
+        # If eaten: not g, check if that unit matches the one in the food database
         else:
-            # Units is the one used in the food database --> Use Size_g
-            if eaten_unit == Size_unit:
-                scale = eaten_size / Size_g
-            # No match --> Error
+            # If it matches, use Size for scaling
+            if eaten_unit == str(df_dummy.loc[0]["Size_unit"]):
+                print(eaten_name)
+                scale = eaten_size / float(df_dummy.loc[0]["Size"])
+            # Else, warning error as there is nothing to use for scaling, the entry in the diary must be fixed
             else:
-                print(f"WARNING UNIT: eaten = {eaten_unit} vs database = {Size_unit}")
+                print(
+                    f"WARNING UNIT: eaten = {eaten_unit} vs database = {df_dummy.loc[0]['Size_unit']}"
+                )
                 continue
+        # Fill the output database entry (dictionary) with scaled factors
+        dict_nutrients["Name"] = eaten_name
+        dict_nutrients["Calories"] = df_dummy.loc[0]["Calories"] * scale
+        dict_nutrients["Total_fats"] = df_dummy.loc[0]["Total_fats"] * scale
+        dict_nutrients["Saturated_fats"] = df_dummy.loc[0]["Saturated_fats"] * scale
+        dict_nutrients["Cholesterol"] = df_dummy.loc[0]["Cholesterol"] * scale
+        dict_nutrients["Sodium"] = df_dummy.loc[0]["Sodium"] * scale
+        dict_nutrients["Total_carbs"] = df_dummy.loc[0]["Total_carbs"] * scale
+        dict_nutrients["Dietary_fibers"] = df_dummy.loc[0]["Dietary_fibers"] * scale
+        dict_nutrients["Sugars"] = df_dummy.loc[0]["Sugars"] * scale
+        dict_nutrients["Proteins"] = df_dummy.loc[0]["Proteins"] * scale
+        dict_nutrients["Meal"] = eaten_meal
+        dict_nutrients["Eaten"] = eaten_size
+        dict_nutrients["Scaling"] = scale
 
-        dict_nutrients = {
-            "Name": Name,
-            "Calories": Calories * scale,
-            "Total_fats": Total_fats * scale,
-            "Saturated_fats": Saturated_fats * scale,
-            "Cholesterol": Cholesterol * scale,
-            "Sodium": Sodium * scale,
-            "Total_carbs": Total_carbs * scale,
-            "Dietary_fibers": Dietary_fibers * scale,
-            "Sugars": Sugars * scale,
-            "Proteins": Proteins * scale,
-        }
-
-        df_nutrients
-
-        new_row = pd.DataFrame([new_data])
-        df = df.append(new_row, ignore_index=True)
+        # Append to the output database
+        df_nutrients.loc[i] = dict_nutrients
+    return df_nutrients
 
 
 # duncion that plots averages of main nutrients
+def sum_up_day(df_nutrients):
+    """
+    This function evaluates all the nutrients eaten in a day by category
+
+    Parameters:
+        df_nutrients (pd.DataFrame): DataFrame with all the food eaten and nutrients in a given diary entry scaled accordingly
+
+    Returns:
+
+    """
+
+    # Sum of each category for the whole day
+    df_total = df_nutrients.sum(axis=0)
+    calories = df_total["Calories"]
+    total_fats = df_total["Total_fats"]
+    saturated_fats = df_total["Saturated_fats"]
+    other_fats = total_fats - saturated_fats
+    cholesterol = df_total["Cholesterol"]
+    sodium = df_total["Sodium"]
+    total_carbs = df_total["Total_carbs"]
+    dietary_fibers = df_total["Dietary_fibers"]
+    sugars = df_total["Sugars"]
+    other_carbs = total_carbs - dietary_fibers - sugars
+    proteins = df_total["Proteins"]
+
+    # Ratios of calories into fat, carbs, prot
+    plot_pie(
+        np.array(
+            [
+                [dietary_fibers, sugars, other_carbs],
+                [saturated_fats, other_fats, 0],
+                [proteins, 0, 0],
+            ]
+        ),
+        calories,
+    )
+
+
+def plot_pie(values, calories):
+    """ """
+    fig, ax = plt.subplots()
+    size = 0.3
+    # Names for the groups and subgroups
+    group_names = ["Carbs", "Fats", "Protein"]
+    subgroup_names = ["Fiber", "Sugar", "Other", "Saturated", "Other", "", "", "", ""]
+    cmap = plt.colormaps["tab20c"]
+    outer_colors = cmap(np.arange(3) * 4)
+    inner_colors = cmap([1, 2, 3, 5, 6, 7, 9, 9, 9])
+
+    # Outer pie (Groups)
+    outer_labels = [
+        f"{name}\n{value:.1f}%"
+        for name, value in zip(group_names, 100 * values.sum(axis=1) / values.sum())
+    ]
+    _, texts = ax.pie(
+        values.sum(axis=1),
+        radius=1 - size,
+        colors=outer_colors,
+        labels=outer_labels,
+        wedgeprops=dict(width=size, edgecolor="w"),
+        labeldistance=0.65,
+    )
+
+    # Make the labels better readable
+    for text in texts:
+        text.set_fontsize(9)
+
+    # Inner pie (Subgroups)
+    inner_labels = []
+    for i, name in enumerate(subgroup_names):
+        if i in [0, 1, 2]:
+            total = values.sum(axis=1)[0]
+        elif i in [3, 4, 5]:
+            total = values.sum(axis=1)[1]
+        elif i in [6, 7, 8]:
+            total = values.sum(axis=1)[2]
+
+        if values.flatten()[i] > 0:
+            inner_labels.append(f"{name}\n{100 * values.flatten()[i]/total:.1f}%")
+        else:
+            inner_labels.append("")
+
+    _, texts = ax.pie(
+        values.flatten(),
+        radius=1,
+        colors=inner_colors,
+        labels=inner_labels,
+        wedgeprops=dict(width=size, edgecolor="w"),
+        labeldistance=1.05,
+    )
+
+    # Make the labels better readable
+    for text in texts:
+        text.set_fontsize(8)
+
+    ax.set(aspect="equal", title=f"{int(calories)} kcal")
+    plt.show()
+
+
 # pie chart?
 # pick a date and show nutritional details
-
-
-def load_food_database(file_path=database_path):
-    df = pd.read_csv(file_path)
-    # Convert numpy.float64('nan') to np.nan explicitly (redundant but safe for demonstration)
-    for col in df.columns:
-        df[col] = df[col].apply(lambda x: np.nan if pd.isna(x) else x)
-    return df
-
-
-def save_df(df, file_path=database_path):
-    df.to_csv(file_path, index=False)
+# Sum of each category for each meal of day
 
 
 def save_entry_to_database(file_path, entry, category):
